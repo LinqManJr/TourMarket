3f60bdb58eba48b57292d4ae1c4dbb9f0f986e2e
LinqManJr <johnybond32@yandex.ru>
2019-11-11 23:50:50 +0900

    Add MarketContext

---
+++ Context/MarketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TourMarket.Models;
+
+namespace TourMarket.Context
+{
+    public class MarketContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Tourist> Tourists { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+    }
+}
