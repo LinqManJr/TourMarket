4654f31bbfe18ef24eceaea059175c62c87734b2
LinqManJr <johnybond32@yandex.ru>
2019-11-11 23:30:30 +0900

    add model Tourist

---
+++ Models/Tourist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMarket.Models
+{
+    public class Tourist
+    {
+        public int Id { get; set; }
+        public string Fio { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
