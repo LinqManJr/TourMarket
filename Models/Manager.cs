8720fdbb4e74a9969d6e6a1320108f3498327f1b
LinqManJr <johnybond32@yandex.ru>
2019-11-11 23:32:17 +0900

    add Manager model

---
+++ Models/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMarket.Models
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
