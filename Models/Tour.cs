688985cb4ede1d78f10f28a9948e395e1c7687f6
LinqManJr <johnybond32@yandex.ru>
2019-11-11 23:28:57 +0900

    add model Tour

---
+++ Models/Tour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMarket.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+        public double Price { get; set; }
+    }
+}
