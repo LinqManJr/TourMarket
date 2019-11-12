d3b9a119b57e74f1a3a64973dbe0e4e2e18bcc9a
LinqManJr <johnybond32@yandex.ru>
2019-11-12 11:11:07 +0900

    Create IRepository interface

---
+++ Context/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMarket.Context
+{
+    interface IRepository<TEntity> where TEntity:class
+    {
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        TEntity Create(TEntity entity);
+        TEntity FindById(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
