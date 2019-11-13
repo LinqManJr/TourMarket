10158cd957acd2326937c495e98773deb5697f5d
LinqManJr <johnybond32@yandex.ru>
2019-11-13 23:31:52 +0900

    almost complete accountcontroller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TourMarket.Dto;
+using TourMarket.Helpers;
 using TourMarket.Models;
 using TourMarket.Services;
 
@@ -21,23 +27,47 @@ namespace TourMarket.Controllers
             this.userService = userService;
         }
 
-        [HttpPost("[action]")]
-        public IActionResult Login([FromBody]Manager manager)
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]ManagerDto managerDto)
         {
-            //TODO: managerDTO
-            throw new NotImplementedException();
+            var manager = userService.Authenticate(managerDto.Login, managerDto.Password);
+
+            if (manager == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, manager.Login.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+                        
+            return Ok(new
+            {
+                Id = manager.Id,
+                Login = manager.Login,
+                Name = manager.Name,
+                Token = tokenString
+            });
         }
 
         [AllowAnonymous]
         [HttpPost("register")]
-        public IActionResult Register([FromBody]UserDto userDto)
+        public IActionResult Register([FromBody]ManagerDto managerDto)
         {            
-            var user = _mapper.Map<User>(userDto);
+            var user = managerDto.ToManager();
 
             try
-            {
-                
-                userService.Create(user, userDto.Password);
+            {                
+                userService.Create(user, managerDto.Password);
                 return Ok();
             }
             catch (Exception ex)
