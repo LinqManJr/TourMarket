251c87ada4ba1d11953f66affced630834cf37d3
LinqManJr <johnybond32@yandex.ru>
2019-11-14 17:33:28 +0900

    add managerService

---
+++ ClientApp/src/app/_services/Manager.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { Manager } from '../_models';
+
+@Injectable()
+export class ManagerService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<Manager[]>(`${environment.apiUrl}/managers`);
+    }
+
+    getById(id: number) {
+        return this.http.get(`${environment.apiUrl}/managers/` + id);
+    }
+
+    register(manager: Manager) {
+        return this.http.post(`${environment.apiUrl}/managers/register`, user);
+    }
+
+    update(manager: Manager) {
+        return this.http.put(`${environment.apiUrl}/managers/` + manager.id, manager);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${environment.apiUrl}/managers/` + id);
+    }
+}
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+}*/
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { Manager } from '../_models';
+import { UserService } from '../_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+  currentManager: Manager;
+  managers: Manager[] = [];
+
+  constructor(private userService: UserService) {
+    this.currentManager = JSON.parse(localStorage.getItem('currentManager'));
+  }
+
+  ngOnInit() {
+    this.loadAllManagers();
+  }
+
+  deleteUser(id: number) {
+    this.ManagerService.delete(id).pipe(first()).subscribe(() => {
+      this.loadAllManagers()
+    });
+  }
+
+  private loadAllUsers() {
+    this.userService.getAll().pipe(first()).subscribe(managers => {
+      this.managers = managers;
+    });
+  }
 }
