4f4b35efdc86fba77cdddcbb97a9659700998792
LinqManJr <johnybond32@yandex.ru>
2019-11-18 00:33:00 +0900

    add model orderDto

---
+++ Dto/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMarket.Dto
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string TourName { get; set; }
+        public double TourPrice { get; set; }
+        public string ManagerName { get; set; }
+        public string TouristName { get; set; }
+    }
+}
