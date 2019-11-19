041d942c8b73c2d990fe2763ee540c49e81d9e23
LinqManJr <johnybond32@yandex.ru>
2019-11-19 22:56:02 +0900

    remove models OrderManager, OrderTourist remove code in ModelCreating

--- Context/MarketContext.cs
+++ Context/MarketContext.cs
@@ -15,30 +15,8 @@ namespace TourMarket.Context
         {
 
             builder.Entity<Order>().HasKey(t => t.Id);
-            builder.Entity<Tourist>().HasKey(t => t.Id);
-
-            builder.Entity<OrderManager>()
-                .HasKey(t => new { t.OrderId, t.ManagerId });
-
-            builder.Entity<OrderManager>()
-                .HasOne(t => t.Manager)
-                .WithMany(m => m.OrderManagers)
-                .HasForeignKey(t => t.ManagerId);
-            builder.Entity<OrderManager>()
-                .HasOne(t => t.Order)
-                .WithMany(m => m.OrderManagers)
-                .HasForeignKey(t => t.OrderId);
-
-            builder.Entity<OrderTourist>()
-                .HasKey(t => new { t.OrderId, t.TouristId});
-            builder.Entity<OrderTourist>()
-                .HasOne(t => t.Order)
-                .WithMany(m => m.OrderTourists)
-                .HasForeignKey(t => t.OrderId);
-            builder.Entity<OrderTourist>()
-                .HasOne(t => t.Tourist)
-                .WithMany(m => m.OrderTourists)
-                .HasForeignKey(t => t.TouristId);
+            builder.Entity<Tourist>().HasKey(t => t.Id);   
+                                 
 
             //seeding somedata
             builder.Entity<Tour>().HasData(
@@ -58,29 +36,12 @@ namespace TourMarket.Context
                 new Order { Id = 2, Date = DateTime.Now.AddDays(-1), TourId = 2 },
                 new Order { Id = 3, Date = DateTime.Now.AddDays(-2), TourId = 3 },
                 new Order { Id = 4, Date = DateTime.Now.AddDays(-3), TourId = 4 }
-                );
-
-            builder.Entity<OrderManager>().HasData(
-                new OrderManager { OrderId = 1, ManagerId = 1},
-                new OrderManager { OrderId = 2, ManagerId = 1},
-                new OrderManager { OrderId = 3, ManagerId = 1},
-                new OrderManager { OrderId = 4, ManagerId = 1}
-                );
-
-            builder.Entity<OrderTourist>().HasData(
-                new OrderTourist { OrderId = 1, TouristId = 1},
-                new OrderTourist { OrderId = 2, TouristId = 2 },
-                new OrderTourist { OrderId = 3, TouristId = 3 },
-                new OrderTourist { OrderId = 4, TouristId = 4 }
-                );
+                );            
         }
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<Tour> Tours { get; set; }
         public DbSet<Tourist> Tourists { get; set; }
-        public DbSet<Manager> Managers { get; set; }
-
-        public DbSet<OrderManager> OrderManagers { get; set; }
-        public DbSet<OrderTourist> OrderTourist { get; set; }
+        public DbSet<Manager> Managers { get; set; }        
     }
 }
