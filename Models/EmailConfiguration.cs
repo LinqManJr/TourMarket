275896441a938d118c05152f8c4bad3f5148b175
LinqManJr <johnybond32@yandex.ru>
2019-11-22 23:24:44 +0900

    Add EmailConfiguration class

---
+++ Models/EmailConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace TourMarket.Models
+{
+    public class EmailConfiguration
+    {
+        public string SmtpServer { get; }
+        public int Port { get; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public EmailConfiguration(string server, int port, string user, string pass)
+        {
+            SmtpServer = server;
+            Port = port;
+            Username = user;
+            Password = pass;
+        }
+    }
+}
