b636c8a47d6a2bdb78909a3e0871a00f4530fdf9
LinqManJr <johnybond32@yandex.ru>
2019-11-22 23:20:30 +0900

    add EmailSender class without content

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit;
+
+namespace TourMarket.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string from, string to, string message);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string from, string to, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
