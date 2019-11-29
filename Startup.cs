c78e1e62dea3ff860de69706d5201320b9562a48
LinqManJr <johnybond32@yandex.ru>
2019-11-30 00:02:44 +0900

    remove spa functionality from startup

--- Startup.cs
+++ Startup.cs
@@ -98,20 +98,7 @@ namespace TourMarket
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
-                .AllowAnyHeader());
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
+                .AllowAnyHeader());            
         }
     }
 }
