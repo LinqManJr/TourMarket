6a6d7d52005025703c2375951657ffc594d09024
LinqManJr <johnybond32@yandex.ru>
2019-12-10 22:07:48 +0900

    remove undefined usings

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using TourMarket.Dto;
 using TourMarket.Models;
-using TourMarket.Services;
 
 namespace TourMarket.Helpers
 {
