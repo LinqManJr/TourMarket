13a269eb885d943298b003416571c97caa0bc507
LinqManJr <johnybond32@yandex.ru>
2019-12-10 23:56:14 +0900

    add swagger comments to OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TourMarket.Context;
 using TourMarket.Dto;
@@ -13,7 +14,9 @@ namespace TourMarket.Controllers
 {
     [Route("api/[controller]")]
     [Authorize]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class OrderController : ControllerBase
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     {
         private readonly OrderRepository _repository;
 
@@ -22,7 +25,15 @@ namespace TourMarket.Controllers
             _repository = repository;
         }
 
-        [HttpGet("[action]")]        
+        /// <summary>
+        /// Return Orders By Authorize Manager
+        /// </summary>
+        /// <returns>IEnumerable of Orders</returns>
+        /// <response code="200">Returns Ok with orders data</response>
+        /// <response code="404">If order count is 0</response>
+        [HttpGet("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IQueryable<Order>> GetOrdersByManager()
         {
             var managerId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
@@ -34,7 +45,15 @@ namespace TourMarket.Controllers
             return Ok(orders);
         }
 
+        /// <summary>
+        /// Return all orders in database
+        /// </summary>
+        /// <returns>IQueryable of Orders</returns>
+        /// <response code="200">Returns Ok with orders data</response>
+        /// <response code="404">If order count is 0</response>
         [HttpGet("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IQueryable<Order>> GetOrders()
         {
             var orders = _repository.GetOrders();
@@ -45,17 +64,35 @@ namespace TourMarket.Controllers
             return Ok(orders);
         }
 
+        /// <summary>
+        /// Add new Order
+        /// </summary>
+        /// <param name="order">Order entity</param>
+        /// <returns>ActionResult with Order data</returns>
+        /// <response code="200">Returns Ok with order data</response>
+        /// <response code="400">If ModelState is Valid</response>
         [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<Order> AddOrder([FromBody]Order order)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
            
             _repository.Create(order);
-            return order;
+            return Ok(order);
         }
 
+        /// <summary>
+        /// Remove Order
+        /// </summary>
+        /// <param name="order">Order</param>
+        /// <returns>ActionResult (204 or 404)</returns>
+        /// <response code="204">If Remove is true</response>
+        /// <response code="404">If order is null</response>
         [HttpDelete("[action]")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult Remove([FromBody]Order order)
         {
             if (!_repository.IfExist(order))
@@ -65,14 +102,23 @@ namespace TourMarket.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Update Order
+        /// </summary>
+        /// <param name="order">Order</param>
+        /// <returns>ActionResult with order data</returns>
+        /// <response code="200">Returns Ok with order data</response>        
+        /// <response code="404">If order is null</response>
         [HttpPut("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]        
         public ActionResult<Order> Update([FromBody]Order order)
         {
             if (!_repository.IfExist(order))
                 return NotFound("Order not exist");
 
             _repository.Update(order);
-            return order;
+            return Ok(order);
         }
     }
 }
